f0158ed4a3a1f66ce8e3eaf31034432a711f58d4
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-05-28 16:44:22 -0600

    Update DeEdger.bat

--- DeEdger.bat
+++ DeEdger.bat
@@ -5,7 +5,7 @@ echo This program will uninstall Microsoft Edge.
 echo Make sure to run it with administrator privliges.
 pause
 taskkill /f /im msedge.exe
-Remove-Item "C:\Program Files (x86)\Microsoft\Edge"
-Remove-Item "C:\Program Files (x86)\Microsoft\EdgeCore"
-Remove-Item "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+del /f "C:\Program Files (x86)\Microsoft\Edge"
+del /f "C:\Program Files (x86)\Microsoft\EdgeCore"
+del /f "C:\Program Files (x86)\Microsoft\EdgeUpdate"
 pause
