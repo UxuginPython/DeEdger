11eea32574bf5b4efd865832649e4eaeed9a09a2
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-05-28 20:30:26 -0600

    Update DeEdger.bat

--- DeEdger.bat
+++ DeEdger.bat
@@ -2,7 +2,8 @@
 @echo off
 echo UxuginPython DeEdger 1.1.0
 echo This program will uninstall Microsoft Edge.
-echo Make sure to run it with administrator privliges.
+echo Make sure to run it with administrator privileges.
+echo Confirm that you have another browser installed before proceeding.
 pause
 taskkill /f /im msedge.exe
 rmdir "C:\Program Files (x86)\Microsoft\Edge" /s /q
