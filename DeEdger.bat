47341fc1c6274aa2335477740bf29f52876492cd
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-05-28 20:24:20 -0600

    Update DeEdger.bat

--- DeEdger.bat
+++ DeEdger.bat
@@ -1,6 +1,6 @@
 :: UxuginPython DeEdger 1.1.0
 @echo off
-echo UxuginPython DeEdger 1.0.0-InDev
+echo UxuginPython DeEdger 1.1.0
 echo This program will uninstall Microsoft Edge.
 echo Make sure to run it with administrator privliges.
 pause
