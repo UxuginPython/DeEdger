0764e2b825865ec2bb53a504d41fcdd53aefd990
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2024-02-23 03:12:31 -0700

    Update DeEdger.bat

--- DeEdger.bat
+++ DeEdger.bat
@@ -5,6 +5,7 @@ echo Make sure to run it with administrator privileges.
 echo Confirm that you have another browser installed before proceeding.
 pause
 taskkill /f /im msedge.exe
+taskkill /f /im MicrosoftEdgeUpdate.exe
 rmdir "C:\Program Files (x86)\Microsoft\Edge" /s /q
 rmdir "C:\Program Files (x86)\Microsoft\EdgeCore" /s /q
 rmdir "C:\Program Files (x86)\Microsoft\EdgeUpdate" /s /q
